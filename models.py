ef0db981dd194184bad60e0676aa80c78b8c5793
Juhi Chaudhari <juhichaudhari@gmail.com>
2021-04-11 22:38:56 +0000

    added database models

---
+++ models.py
@@ -0,0 +1,22 @@
+from app import db
+
+class Person(db.Model): 
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), nullable=False)
+
+    def __repr__(self): 
+        return '<User %r>' % self.username
+
+class Contact(db.Model): 
+    __tablename__ = 'contacts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    emails = db.Column(db.String(30), nullable=False)
+    priorityLevel = db.Column(db.Integer, nullable=False)
+    person_id = db.Column(db.Integer, db.ForeignKey('person.id'), nullable=False)
+
+    def __repr__(self): 
+        return '<Contact %r>' % self.name
