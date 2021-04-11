64392fb54088cbdfb08e73f8242a289b5be8669c
EC2 Default User <kathlyn.hang.nguyen11@gmail.com>
2021-04-11 17:53:44 +0000

    Add react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import './css/App.css';
 
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import LandingPage from './components/LandingPage';
+import LoginPage from './components/LoginPage';
+import ContactBook from './components/ContactBook';
 
 function App() {
   return (
@@ -14,6 +23,33 @@ function App() {
           crossOrigin="anonymous"
         />
       </head>
+      <body>
+        <h1 className="jumbotron-heading">App main page</h1>
+        <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/login">Log in here</Link>
+            </li>
+            <li>
+              <Link to="/landing">Landing</Link>
+            </li>
+            <li>
+              <Link to="/contact-book">Contact Book</Link>
+            </li>
+          </ul>
+        </nav>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/login" exact component={LoginPage}/>
+          <Route path="/landing" exact component={LandingPage}/>
+          <Route path="/contact-book" exact component={ContactBook}/>
+        </Switch>
+      </div>
+    </Router>
+      </body>
     </div>
   );
 }
