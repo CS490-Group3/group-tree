513c2887c2e96985c00781d95de865db15363d42
EC2 Default User <kathlyn.hang.nguyen11@gmail.com>
2021-04-11 18:03:44 +0000

    Add placeholder in contact book

--- src/components/ContactBook.jsx
+++ src/components/ContactBook.jsx
@@ -2,6 +2,39 @@ import React from 'react'
 
 export default function ContactBook() {
     return (
-        <h1>Contact Book</h1>
+        <div>
+            <h2>Contact Book</h2>
+            <table className="table table-hover">
+                <thead>
+                    <tr className="table-success">
+                        <th scope="col">#</th>
+                        <th scope="col">First</th>
+                        <th scope="col">Last</th>
+                        <th scope="col">Handle</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                       <td>Mark</td>
+                       <td>Otto</td>
+                       <td>@mdo</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>Larry the Bird</td>
+                        <td>Larry</td>
+                        <td>@twitter</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        
   );
 }
