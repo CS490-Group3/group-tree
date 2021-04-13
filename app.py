9bf6a01e63bbcfce074659e0f9f44b9591e6b4c3
Benjamin-Hayduchok <77507585+Benjamin-Hayduchok@users.noreply.github.com>
2021-04-13 18:50:24 -0400

    Database (#23)
    * added database models
    
    * Added working functionality to database declaration for contacts and person tables, (Juhi and Ben)
    
    * Updated formatting on python files to allow for 10/10 for pylint
    
    * Added slight changes to app.py to attempt 10/10 pylint on GitHub side
    
    * added .env and sql.env to gitignore
    
    Co-authored-by: Juhi Chaudhari <juhichaudhari@gmail.com>
    Co-authored-by: EC2 Default User <ec2-user@ip-172-31-90-99.ec2.internal>

--- app.py
+++ app.py
@@ -1,23 +1,29 @@
 """
 Template Flask app
 """
-
+# pylint: disable=import-error
 import os
 from flask import Flask, send_from_directory
-
-app = Flask(__name__, static_folder="./build/static")
-
-
-@app.route("/", defaults={"filename": "index.html"})
-@app.route("/<path:filename>")
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())  # This is to load env variables from .env
+APP = Flask(__name__, static_folder="./build/static")
+# Point SQLAlchemy to Heroku database
+APP.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+# Gets rid of a warning
+APP.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+DB = SQLAlchemy(APP)
+# IMPORTANT: This must be AFTER creating db variable to prevent
+# circular import issues
+DB.create_all()
+@APP.route("/", defaults={"filename": "index.html"})
+@APP.route("/<path:filename>")
 def index(filename):
     """
     Serves files from ./build
     """
     return send_from_directory("./build", filename)
-
-
-app.run(
+APP.run(
     host=os.getenv("IP", "0.0.0.0"),
     port=8081 if os.getenv("C9_PORT") else int(os.getenv("PORT", "8081")),
 )
