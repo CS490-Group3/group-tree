33a37b678b0eb94a6d7836cda1923ed75007c90c
Andrew Kritzler <10812727+peppermintpatty5@users.noreply.github.com>
2021-04-15 01:30:42 +0000

    Login tied in with server endpoint

--- app.py
+++ app.py
@@ -3,9 +3,40 @@ Template Flask app
 """
 
 import os
-from flask import Flask, send_from_directory
+
+from flask import Flask, request, send_from_directory
+import requests
+
+GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
+GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
 
 app = Flask(__name__, static_folder="./build/static")
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    """
+    Endpoint for authenticating with Google's login API.
+    """
+    request_data = request.get_json()
+
+    if request_data:
+        token = request_data.get("token")
+
+        # ask Google to validate the token, instead of doing it manually
+        google_response = requests.get(
+            "https://www.googleapis.com/oauth2/v3/tokeninfo", {"id_token": token}
+        )
+
+        if google_response:  # not error
+            profile = google_response.json()
+            sub = profile["sub"]  # can use as primary key
+            name = profile["name"]
+            email = profile["email"]
+
+        print(sub, name, email)
+
+    return {}
 
 
 @app.route("/", defaults={"filename": "index.html"})
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,29 +1,24 @@
-import React, { useState } from 'react';
-import { GoogleLogin, GoogleLogout } from 'react-google-login';
+import React from 'react';
+import { GoogleLogin } from 'react-google-login';
 
 const clientId = '1021307606256-ktn0m9dj0bn27nc2qei22m42h9dj02dk.apps.googleusercontent.com';
 
 function Login() {
-  const [loggedIn, setLoggedIn] = useState(false);
-
   return (
     <div>
       <GoogleLogin
         clientId={clientId}
         onSuccess={(response) => {
-          console.log('Login success', response);
+          fetch('/login', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ token: response.tokenId }),
+          });
         }}
-        onFailure={(response) => {
-          console.log('Login failure', response);
-        }}
+        onFailure={() => {}}
         cookiePolicy="single_host_origin"
-        // isSignedIn
-      />
-      <GoogleLogout
-        clientId={clientId}
-        onLogoutSuccess={(response) => {
-          console.log('Logout success', response);
-        }}
       />
     </div>
   );
