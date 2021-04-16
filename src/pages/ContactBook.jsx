252a80c3505ae517f5e411f31ee3827f79b03dea
Juhi Chaudhari <46832246+juhichaudhari@users.noreply.github.com>
2021-04-15 23:22:45 -0400

    User is able to add contact information (#35)
    * added popup on click button where user can add contact info and populate table with userState rather than static info
    
    * updated readme with requirements
    
    * update the contacts table once user successfully submit form

--- src/pages/ContactBook.jsx
+++ src/pages/ContactBook.jsx
@@ -1,41 +1,81 @@
-import React from 'react';
+import React, { useState }  from 'react'
+import '../css/ContactBook.css';
+import "react-responsive-modal/styles.css";
+import { Modal } from 'react-responsive-modal';
+import { useForm } from 'react-hook-form';
 
-function ContactBook() {
-  return (
-    <div>
-      <h2>Contact Book</h2>
-      <table className="table table-hover">
-        <thead>
-          <tr className="table-success">
-            <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td>Larry the Bird</td>
-            <td>Larry</td>
-            <td>@twitter</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+/* eslint-disable react/jsx-props-no-spreading */
+
+export default function ContactBook() {
+    const [open, setOpen] = useState(false);
+    const onOpenModal = () => setOpen(true);
+    const onCloseModal = () => setOpen(false);
+    
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+    
+    const [contacts, setContacts] = useState([
+        { name: 'Chris Doe', email:"@gmail", phoneNumber: "732-344-0000"},
+        { name: 'Nick Doe', email:"@gmail", phoneNumber: "732-344-0000"},
+        { name: 'Jacov Doe', email:"@gmail", phoneNumber: "732-344-0000"}
+    ]);
+  
+    function onSubmit(data) {
+        console.log(data);
+        setContacts(copyContacts => [...copyContacts, data])
+    }
+  
+    return (
+        <div>
+            <h2 className="text-center">Contact Book</h2>
+            <div className="text-right">
+              <button onClick={onOpenModal} type="button"  className="add-button">Add New Contact</button>
+            </div>
+            <table className="table table-hover">
+                <thead>
+                    <tr className="table-success">
+                        <th scope="col">Name</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Phone Number</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {contacts.map(c => (
+                      <tr>
+                        <th>{c.name}</th>
+                        <td>{c.email}</td>
+                        <td>{c.phoneNumber}</td>
+                    </tr>
+                  ))}
+                </tbody>
+            </table>
+            
+            <div>
+              <Modal open={open} onClose={onCloseModal} center>
+                <h2>Edit Contact</h2>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                
+                    Name:
+                    <input type="text" placeholder="Name" {...register("name", {required: true, maxLength: 80})} />
+                    {errors.name && <p>Name is required.</p>}
+                  
+                    Email:
+                    <input type="text" placeholder="Email" {...register("email", {required: true, pattern: /^\S+@\S+$/i})} />
+                    {errors.email && <p>Please enter valid email.</p>}
+
+                    Phone Number:
+                    <input type="tel" placeholder="Mobile number" {...register("phoneNumber", {required: true, maxLength: 12})} />
+                    {errors.phoneNumber && <p>Plese Enter valid phone number</p>}
+                    
+                  <input type="submit" />
+                  
+                </form>
+              </Modal>
+            </div>
+        </div>
+        
   );
 }
-
-export default ContactBook;
