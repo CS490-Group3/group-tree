498ac5347d009ebdabe7c23e50cd51d3ab9fcdb4
Juhi Chaudhari <46832246+juhichaudhari@users.noreply.github.com>
2021-04-16 13:47:17 -0400

    Database fix (#37)
    * made database working(code provided by Ben)
    
    * added functions to add user and contact to database
    
    * pylint fix
    
    * solving pylint issues
    
    * pylint fix
    
    * created exts,py to fix circular import issues
    
    * fixing pylint error
    
    * small fix

---
+++ exts.py
@@ -0,0 +1,17 @@
+''' helper file to prevent circular import issue'''
+
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+app = Flask(__name__, static_folder="./build/static")
+# Point SQLAlchemy to Heroku database
+
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+# Gets rid of a warning
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
