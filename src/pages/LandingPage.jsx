2ad97d2c947cd324118511426610de38c43da7d3
Kathlyn Hang Nguyen <kathlyn.hang.nguyen11@gmail.com>
2021-04-16 13:41:50 -0400

    Calendar UI (#39)
    * Add calendar component
    
    * Add styling to app
    
    * Add styling to app
    
    * Add styling to app
    
    * Add Upcoming events view
    
    * Add form and button for new events
    
    * Change layout for landing page
    
    * Add UI for calendar and add new events
    
    * Add styling to add new event button
    
    * Add function to filter unique activities

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,7 +1,94 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Calendar } from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import EVENT_DATA from '../assets/EventData';
 
-function LandingPage() {
-  return <h1>Landing Page</h1>;
+export default function LandingPage() {
+  const [value, onChange] = useState(new Date());
+  function onClickDay(date) {
+    console.log(date);
+    console.log(EVENT_DATA);
+  }
+  const [activityList, setList] = useState([]);
+
+  function updateActivityList() {
+    const unique = [...new Set(EVENT_DATA.map((item) => item.activity))]; // [ 'A', 'B']
+    setList(unique);
+  }
+
+  useEffect(() => {
+    updateActivityList();
+
+    console.log('loaded!');
+    console.log(activityList);
+  });
+
+  return (
+    <div className="landing">
+      <div className="container">
+        <div className="item">
+          <h3>Calendar View</h3>
+          <Calendar onChange={onChange} onClickDay={onClickDay} value={value} />
+        </div>
+        <div className="item">
+          <h3>Upcoming events</h3>
+          <ul className="list-group">
+            <li className="list-group-item ">Cras justo odio</li>
+            <li className="list-group-item">Dapibus ac facilisis in</li>
+            <li className="list-group-item">Morbi leo risus</li>
+            <li className="list-group-item">Porta ac consectetur ac</li>
+            <li className="list-group-item">Vestibulum at eros</li>
+          </ul>
+        </div>
+      </div>
+      <div className="container-form">
+        <div className="item">
+          <form>
+            <div className="form-row">
+              <div className="col center">
+                <label htmlFor="exampleTextarea">
+                  Person
+                  <input type="text" className="form-control" placeholder="Person" />
+                </label>
+              </div>
+              <div className="col center">
+                <label htmlFor="example-date-input">
+                  Date
+                  <input
+                    className="form-control"
+                    type="date"
+                    placeholder="Date"
+                    id="example-date-input"
+                  />
+                </label>
+              </div>
+              <div className="col center">
+                <label htmlFor="exampleSelect1">
+                  Activity
+                  <select
+                    className="form-control"
+                    id="exampleSelect1"
+                    placeholder="Activity"
+                  >
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                  </select>
+                </label>
+              </div>
+              <div className="col center">
+                <input
+                  className=" form-control btn btn-primary"
+                  type="submit"
+                  value="Add new event"
+                />
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
-
-export default LandingPage;
