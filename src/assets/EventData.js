8eddcfc6a17526855ca72fcbcdcfbcfab8cf4e56
EC2 Default User <kathlyn.hang.nguyen11@gmail.com>
2021-04-19 22:43:55 +0000

    Fix eventdata

--- src/assets/EventData.js
+++ src/assets/EventData.js
@@ -9,37 +9,25 @@ const EVENT_DATA = [
     id: 2,
     person: 'Adam',
     date: '',
-<<<<<<< HEAD
     activity: 'cooking',
-=======
->>>>>>> d279bfc731fd58f79a6bc01fa1aa2d34964eedc5
   },
   {
     id: 3,
     person: 'Alex',
     date: '',
-<<<<<<< HEAD
     activity: 'dancing',
-=======
->>>>>>> d279bfc731fd58f79a6bc01fa1aa2d34964eedc5
   },
   {
     id: 4,
     person: 'Tim',
     date: '',
-<<<<<<< HEAD
     activity: 'reading',
-=======
->>>>>>> d279bfc731fd58f79a6bc01fa1aa2d34964eedc5
   },
   {
     id: 5,
     person: 'Kate',
     date: '',
-<<<<<<< HEAD
     activity: 'dancing',
-=======
->>>>>>> d279bfc731fd58f79a6bc01fa1aa2d34964eedc5
   },
 ];
 
