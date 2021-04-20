81285c02d6389a553c4ee14d3b5dc62cd6b2371e
Andrew Kritzler <10812727+peppermintpatty5@users.noreply.github.com>
2021-04-20 15:41:43 +0000

    Refactored using inline expressions
    Removed extraneous <div> tags

--- src/App.jsx
+++ src/App.jsx
@@ -9,70 +9,62 @@ import './css/App.css';
 const clientId = process.env.REACT_APP_CLIENT_ID;
 
 function App() {
-  const [loggedIn, setLoggedIn] = useState(false); // initialize logged in status to fasle
-
-  if (loggedIn) {
-    return (
-      <div className="App">
-        <Router>
-          <div>
-            <nav>
-              <ul>
-                <li>
-                  <Link to="/landing">Landing</Link>
-                </li>
-                <li>
-                  <Link to="/contact-book">Contact Book</Link>
-                </li>
-                <li>
-                  <div>
-                    <GoogleLogout
-                      clientId={clientId}
-                      buttonText="Logout"
-                      onLogoutSuccess={() =>
-                        fetch('/logout', { method: 'POST' })
-                          .then((response) => response.json())
-                          .then((data) => {
-                            if (data.success === true) setLoggedIn(false);
-                          })
-                      }
-                      style={{ marginTop: '100px' }}
-                    />
-                  </div>
-                </li>
-              </ul>
-            </nav>
-            {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-            <Switch>
-              <Route exact path="/landing" component={LandingPage} />
-              <Route path="/contact-book" exact component={ContactBook} />
-            </Switch>
-          </div>
-        </Router>
-      </div>
-    );
-  }
+  const [loggedIn, setLoggedIn] = useState(false);
 
   return (
-    <div>
-      <GoogleLogin
-        clientId={clientId}
-        onSuccess={(googleResponse) =>
-          fetch('/login', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ token: googleResponse.tokenId }),
-          })
-            .then((response) => response.json())
-            .then((data) => {
-              if (data.success === true) setLoggedIn(true);
+    <div className="App">
+      {loggedIn === true ? (
+        <Router>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/landing">Landing</Link>
+              </li>
+              <li>
+                <Link to="/contact-book">Contact Book</Link>
+              </li>
+              <li>
+                <GoogleLogout
+                  clientId={clientId}
+                  buttonText="Logout"
+                  onLogoutSuccess={() =>
+                    fetch('/logout', { method: 'POST' })
+                      .then((response) => response.json())
+                      .then((data) => {
+                        if (data.success === true) setLoggedIn(false);
+                      })
+                  }
+                  style={{ marginTop: '100px' }}
+                />
+              </li>
+            </ul>
+          </nav>
+          {/* A <Switch> looks through its children <Route>s and renders the first one
+          that matches the current URL. */}
+          <Switch>
+            <Route exact path="/landing" component={LandingPage} />
+            <Route path="/contact-book" exact component={ContactBook} />
+          </Switch>
+        </Router>
+      ) : (
+        <GoogleLogin
+          clientId={clientId}
+          onSuccess={(googleResponse) =>
+            fetch('/login', {
+              method: 'POST',
+              headers: { 'Content-Type': 'application/json' },
+              body: JSON.stringify({ token: googleResponse.tokenId }),
             })
-        }
-        onFailure={() => {}}
-        cookiePolicy="single_host_origin"
-        style={{ marginTop: '100px' }}
-      />
+              .then((response) => response.json())
+              .then((data) => {
+                if (data.success === true) setLoggedIn(true);
+              })
+          }
+          onFailure={() => {}}
+          cookiePolicy="single_host_origin"
+          style={{ marginTop: '100px' }}
+        />
+      )}
     </div>
   );
 }
