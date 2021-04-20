1523321dbf92dcc398e794b44ae62a09416f7518
Juhi Chaudhari <juhichaudhari@gmail.com>
2021-04-20 23:03:49 +0000

    2 client side tests done

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -3,10 +3,19 @@ import App from './App';
 import ContactBook from './pages/ContactBook';
 
 test('add new contact click function', () => {
-  const result = render(<ContactBook />);
+  render(<ContactBook />);
   const addnewBtnElement = screen.getByText('Add New Contact');
   expect(addnewBtnElement).toBeInTheDocument();
-  const formElement = document.getElementById("Submit");
+  const formElement = screen.getByText('Phone Number');
   fireEvent.click(addnewBtnElement);
-  expect(addnewBtnElement).toBeInTheDocument();
+  expect(formElement).toBeInTheDocument();
+});
+
+test('contactbook appears', () => {
+  render(<ContactBook />);
+  const contactbookElement = screen.getByText('Contact Book');
+  expect(contactbookElement).toBeInTheDocument();
+  const pageElement = screen.getByText('Name');
+  fireEvent.click(contactbookElement);
+  expect(pageElement).toBeInTheDocument();
 });
