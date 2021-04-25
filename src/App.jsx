8bc4e368a3ac34dc891770947eaee1602cdc0f85
Juhi Chaudhari <46832246+juhichaudhari@users.noreply.github.com>
2021-04-25 18:26:57 -0400

    Navbar added icons (#80)
    * restructured navbar
    
    * icons added
    
    * navbar aligned to right
    
    * navbar decor
    
    * collapsable navbar working
    
    * using more of bootstrap
    
    * changed title
    
    * calendar misspelling fixed

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { GoogleLogin, GoogleLogout } from 'react-google-login';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 import TreeView from './pages/TreeView';
-import CalenderView from './pages/CalenderView';
+import CalendarView from './pages/CalendarView';
 import ContactBook from './pages/ContactBook';
 import './css/App.css';
 
@@ -16,42 +16,58 @@ function App() {
     <div className="App">
       {loggedIn === true ? (
         <Router>
-          <nav>
-            <ul>
-              <li>
-                <h1 id="title">GroupTree</h1>
-              </li>
-              <li>
-                <Link to="/tree-view">TreeView</Link>
-              </li>
-              <li>
-                <Link to="/calender-view">CalenderView</Link>
-              </li>
-              <li>
-                <Link to="/contact-book">ContactBook</Link>
-              </li>
-              <li>
-                <GoogleLogout
-                  clientId={clientId}
-                  buttonText="Logout"
-                  onLogoutSuccess={() =>
-                    fetch('/logout', { method: 'POST' })
-                      .then((response) => response.json())
-                      .then((data) => {
-                        if (data.success === true) setLoggedIn(false);
-                      })
-                  }
-                  style={{ marginTop: '100px' }}
-                />
-              </li>
-            </ul>
+          <nav className="navbar navbar-expand-md" id="nav">
+            <h1 classnName="navbar-brand">GroupTree</h1>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <i className="fas fa-align-justify fa-2x" />
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="nav navbar-nav ml-auto" id="nav-bar">
+                <li className="nav-item px-4 py-2">
+                  <Link to="/tree-view">
+                    <i className="fab fa-pagelines fa-2x" />
+                  </Link>
+                </li>
+                <li className="nav-item px-4 py-2">
+                  <Link to="/calendar-view">
+                    <i className="fas fa-calendar-alt fa-2x" />
+                  </Link>
+                </li>
+                <li className="nav-item px-4 py-2">
+                  <Link to="/contact-book">
+                    <i className="fas fa-user-friends fa-2x" />
+                  </Link>
+                </li>
+                <li className="nav-item px-4 py-2">
+                  <GoogleLogout
+                    clientId={clientId}
+                    buttonText="Logout"
+                    onLogoutSuccess={() =>
+                      fetch('/logout', { method: 'POST' })
+                        .then((response) => response.json())
+                        .then((data) => {
+                          if (data.success === true) setLoggedIn(false);
+                        })
+                    }
+                    style={{ marginTop: '100px' }}
+                  />
+                </li>
+              </ul>
+            </div>
           </nav>
           {/* A <Switch> looks through its children <Route>s and renders the first one
           that matches the current URL. */}
           <Switch>
             <Route exact path="/tree-view" component={TreeView} />
-
-            <Route exact path="/calender-view" component={CalenderView} />
+            <Route exact path="/calendar-view" component={CalendarView} />
             <Route path="/contact-book" exact component={ContactBook} />
           </Switch>
         </Router>
