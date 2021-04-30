d068ce81df32b28cc29a5cef575e9f24ae591048
Juhi Chaudhari <juhichaudhari@gmail.com>
2021-04-30 03:39:24 +0000

    about section css done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,22 +4,28 @@ import Computer from './Computer';
 export default function About() {
   return (
     <div className="about">
-      <h3>What is it?</h3>
-      <div className="outer">
-        <div className="row">
-          <div className="item">
-            <Computer />
-            <p>Add Your Contacts</p>
-          </div>
-          <div className="item">
-            <Computer />
-            <p>Creates Events and Get Reminders</p>
-            <p>Creates Events and Get Reminders</p>
-          </div>
-          <div className="item">
-            <Computer />
-            <p>Watch your tree grow</p>
-          </div>
+      <h3>How It Works</h3>
+      <div className="about-outer">
+        <div className="about-item">
+          <Computer
+            step="1"
+            p1="Add Your Contacts"
+            p2="Adding Contacts allows you to create events with important people in your life"
+          />
+        </div>
+        <div className="about-item">
+          <Computer
+            step="2"
+            p1="Creates Events"
+            p2="Creat event and get reminders about those event so no event is missed"
+          />
+        </div>
+        <div className="about-item">
+          <Computer
+            step="3"
+            p1="Watch Youur Tree Grow"
+            p2="Be rewareded for staying connected with your family and friends and Watch Your Tree Grow"
+          />
         </div>
       </div>
     </div>
--- src/components/Computer.jsx
+++ src/components/Computer.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function Computer() {
+export default function Computer(props) {
+  const { step, p1, p2 } = props;
+
   return (
     <div className="comp-container">
-      <div className="screen" />
+      <div className="screen">
+        <h3>Step {step}: </h3>
+        <p>{p1}</p>
+        <p>{p2}</p>
+      </div>
       <div className="key">
         <div className="comp-button" />
       </div>
@@ -12,3 +19,9 @@ export default function Computer() {
     </div>
   );
 }
+
+Computer.propTypes = {
+  step: PropTypes.string.isRequired,
+  p1: PropTypes.string.isRequired,
+  p2: PropTypes.string.isRequired,
+};
