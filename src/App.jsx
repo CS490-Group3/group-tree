082bd884c258aab291e46d7acf0df58e3114d6c5
Juhi Chaudhari <46832246+juhichaudhari@users.noreply.github.com>
2021-04-30 11:32:14 -0400

    Landing (#96)
    * three sections
    
    * basic foundation
    
    * added navbar
    
    * landing page setteled
    
    * landing page fixed
    
    * about section css done
    
    * fixed about section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import TreeView from './pages/TreeView';
 import CalendarView from './pages/CalendarView';
 import ContactBook from './pages/ContactBook';
+import About from './components/About';
 import './css/App.css';
 
 const clientId = process.env.REACT_APP_CLIENT_ID;
@@ -72,23 +73,46 @@ function App() {
           </Switch>
         </Router>
       ) : (
-        <GoogleLogin
-          clientId={clientId}
-          onSuccess={(googleResponse) =>
-            fetch('/login', {
-              method: 'POST',
-              headers: { 'Content-Type': 'application/json' },
-              body: JSON.stringify({ token: googleResponse.tokenId }),
-            })
-              .then((response) => response.json())
-              .then((data) => {
-                if (data.success === true) setLoggedIn(true);
-              })
-          }
-          onFailure={() => {}}
-          cookiePolicy="single_host_origin"
-          style={{ marginTop: '100px' }}
-        />
+        <div className="landing-page">
+          <div className="login-button">
+            <nav className="navbar navbar-expand-md" id="nav">
+              <h1 classnName="navbar-brand">GroupTree</h1>
+              <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="nav navbar-nav ml-auto" id="nav-bar">
+                  <li className="nav-item px-4 py-2">
+                    <GoogleLogin
+                      clientId={clientId}
+                      onSuccess={(googleResponse) =>
+                        fetch('/login', {
+                          method: 'POST',
+                          headers: { 'Content-Type': 'application/json' },
+                          body: JSON.stringify({ token: googleResponse.tokenId }),
+                        })
+                          .then((response) => response.json())
+                          .then((data) => {
+                            if (data.success === true) setLoggedIn(true);
+                          })
+                      }
+                      onFailure={() => {}}
+                      cookiePolicy="single_host_origin"
+                      style={{ marginTop: '100px' }}
+                    />
+                  </li>
+                </ul>
+              </div>
+            </nav>
+            <div className="main">
+              <div className="intro">
+                <p className="np">Connect with Family and Friends</p>
+                <p>Grow Your tree</p>
+                <button className="learn-button" type="button">
+                  Scroll To Learn More
+                </button>
+              </div>
+            </div>
+          </div>
+          <About />
+        </div>
       )}
     </div>
   );
