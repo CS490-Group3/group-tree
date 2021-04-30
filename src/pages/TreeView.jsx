fdd245bf4979e2d603e35aaea284c048eb0cedb2
Benjamin-Hayduchok <77507585+Benjamin-Hayduchok@users.noreply.github.com>
2021-04-30 18:44:06 -0400

    add: Added 5 png files to display images. Updated TreeView.jsx to display new images based on array index (#97)

--- src/pages/TreeView.jsx
+++ src/pages/TreeView.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
 export default function TreeView() {
+  const trees = ['1.png', '2.png', '3.png', '4.png', '5.png'];
+  const indexValue = 4;
   return (
     <div className="treeview">
       <p>TreeView</p>
+      <img alt="timer" src={trees[indexValue]} />
     </div>
   );
 }
