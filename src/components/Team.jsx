34adbef3887679e89f94e86520f7c848539cc8cb
Juhi Chaudhari <juhichaudhari@gmail.com>
2021-05-01 00:10:38 +0000

    team and team member components

---
+++ src/components/Team.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import TeamMember from './TeamMember';
+
+export default function Team() {
+  return (
+    <div className="team">
+      <h3>Our Team</h3>
+      <div className="team-container">
+        <div className="team-row">
+          <div className="team-item">
+            <TeamMember />
+          </div>
+          <div className="team-item">
+            <TeamMember />
+          </div>
+        </div>
+        <div className="team-row">
+          <div className="team-item">
+            <TeamMember />
+          </div>
+          <div className="team-item">
+            <TeamMember />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/TeamMember.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function TeamMember() {
+  return (
+    <div className="member-container">
+      <div className="member-item">
+        <p>Icon</p>
+      </div>
+      <div className="member-item">
+        <p>TeamMember Name</p>
+        <p>CEO and Co Founder</p>
+        <p>Fullstack Developer</p>
+      </div>
+    </div>
+  );
+}
