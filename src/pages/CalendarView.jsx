7540cfb999dc77809384e703d04d37527596d6a8
Andrew Kritzler <10812727+peppermintpatty5@users.noreply.github.com>
2021-05-04 11:14:36 -0400

    Notifications (#115)
    * Force update npm
    
    * Installed react-web-notification
    
    npm might be broken at this point, idk
    
    * Spelling, remove console.log
    
    * Dump all notifications on calendar view
    
    * Fix eslint

--- src/pages/CalendarView.jsx
+++ src/pages/CalendarView.jsx
@@ -1,26 +1,49 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Calendar } from 'react-calendar';
+import Notification from 'react-web-notification';
 import 'react-calendar/dist/Calendar.css';
 
 import DateInformation from '../components/DateInformation';
 import InputForm from '../components/InputForm';
 
 function CalendarView() {
-  const [value, setValue] = useState(new Date());
+  const [dateView, setDateView] = useState(new Date());
+  const [eventBlob, setEventBlob] = useState({});
+  const today = new Date();
+
+  const shortISOFormat = (date) => date.toISOString().slice(0, 10);
+
+  const fetchEvents = () => {
+    fetch(`/api/v1/events?date=${shortISOFormat(today)}`, { method: 'GET' })
+      .then((response) => response.json())
+      .then((data) => setEventBlob(data));
+  };
+
+  useEffect(() => {
+    fetchEvents();
+  }, []);
 
   return (
     <div className="landing">
+      {Object.values(eventBlob).flatMap((eventList) =>
+        eventList.map((event) => (
+          <Notification
+            title={`${event.activity} with ${event.contact}`}
+            options={{ body: event.start_time }}
+          />
+        )),
+      )}
       <div className="container">
         <div className="item">
           <h3 className="calendar__header font-weight-bold">Calendar View</h3>
           <Calendar
             calendarType="US"
             className="calendar"
-            onChange={setValue}
-            onClickDay={setValue}
+            onChange={setDateView}
+            onClickDay={setDateView}
           />
         </div>
-        <DateInformation fullDate={value} />
+        <DateInformation fullDate={dateView} />
       </div>
       <div className="container-form text-big text-green font-weight-bold">
         <InputForm />
