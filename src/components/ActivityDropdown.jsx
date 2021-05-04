89d91f96322dfdfdad8681c16057b5a2bab54905
Andrew Kritzler <10812727+peppermintpatty5@users.noreply.github.com>
2021-05-04 00:10:59 +0000

    Fixed activity dropdown

--- src/components/ActivityDropdown.jsx
+++ src/components/ActivityDropdown.jsx
@@ -1,40 +1,30 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import DROPDOWN_DATA from '../assets/DropdownData';
 
 function ActivityDropdown(props) {
   const { onSelectActivity } = props;
-  // const activityList = [...new Set(DROPDOWN_DATA.map((item) => item.activity))];
-  // const [defaultValue, setDefaultValue] = useState(activityList[0]);
   const newActivityList = DROPDOWN_DATA;
-  const [newDefaultValue, setNewDefaultValue] = useState(DROPDOWN_DATA[0].modules[0].key);
-  function handleChange(event) {
-    onSelectActivity(event.target.value);
-    // setDefaultValue(event.target.value);
 
-    setNewDefaultValue(event.target.value);
-  }
+  useEffect(() => {
+    onSelectActivity(newActivityList[0].activities[0]);
+  }, []);
 
   return (
     <label htmlFor="exampleSelect1">
       Activity
-      <link
-        href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.3/css/fontawesome.min.css"
-        rel="stylesheet"
-      />
       <select
         className="form-contro custom-select"
         id="exampleSelect1"
         placeholder="Activity"
-        onChange={(event) => handleChange(event)}
-        value={onSelectActivity(newDefaultValue)}
+        onChange={(event) => onSelectActivity(event.target.value)}
       >
-        {newActivityList.map((activity) => (
-          <optgroup label={activity.key} style={{ color: activity.color }}>
-            {activity.modules.map((item) => (
-              <option value={item.key} selected={newDefaultValue === item.key}>
-                {item.key[0].toUpperCase() + item.key.substring(1)}
+        {newActivityList.map((group) => (
+          <optgroup label={group.category} style={{ color: group.color }}>
+            {group.activities.map((activity) => (
+              <option value={activity}>
+                {activity[0].toUpperCase() + activity.substring(1)}
               </option>
             ))}
           </optgroup>
