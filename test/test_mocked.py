0ea59e8f30613287204f8d735071364ceba78e61
Andrew Kritzler <10812727+peppermintpatty5@users.noreply.github.com>
2021-05-06 05:14:47 +0000

    Sad attempt at mocked test

--- test/test_mocked.py
+++ test/test_mocked.py
@@ -1,3 +1,57 @@
 """
-This file tests nothing
+This module contains unit tests with mocking
 """
+
+import datetime
+import unittest
+from unittest.mock import patch
+
+from app.app import add_new_event, models, User
+
+
+class AddNewEventTest(unittest.TestCase):
+    """Test case for `add_new_event`"""
+
+    def setUp(self):
+        """Set up"""
+        self.mock_user = User(id=1000, name="Bill", tree_points=0)
+        self.mock_events = []
+        self.mock_contacts = {
+            2000: models.Contact(
+                id=2000,
+                name="Dude",
+                email="dude@example.com",
+                phone="123-345-7890",
+                person_id=1000,
+            )
+        }
+
+    def mocked_db_session_add(self, event):
+        """Mocked database function"""
+        self.mock_events.append(event)
+
+    def mocked_db_session_commit(self):
+        """Mocked database function"""
+        pass
+
+    def mocked_contact_query_get(self, contact_id):
+        """Mocked database function"""
+        return self.mock_contacts.get(contact_id)
+
+    @unittest.skip("I don't know what I'm doing")
+    def test_valid(self):
+        """Test on valid input"""
+        with patch("app.app.db.session.add", self.mocked_db_session_add), patch(
+            "app.app.db.session.commit", self.mocked_db_session_commit
+        ), patch("app.app.models.Contact.query.get", self.mocked_contact_query_get):
+            result = add_new_event(
+                {
+                    "activity": "fishing",
+                    "start_time": datetime.datetime.now(),
+                    "period": "7",
+                    "contact_id": "2000",
+                },
+                self.mock_user,
+            )
+            self.assertEqual(result.status_code, 204)
+            self.assertEqual(len(self.mock_events), 1)
