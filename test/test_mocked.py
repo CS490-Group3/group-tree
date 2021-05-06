84e86439f364b5982281dce9a39a19fee401e215
Andrew Kritzler <10812727+peppermintpatty5@users.noreply.github.com>
2021-05-06 13:52:47 +0000

    Bad request test case

--- test/test_mocked.py
+++ test/test_mocked.py
@@ -54,3 +54,21 @@ class AddNewEventTest(unittest.TestCase):
             )
             self.assertEqual(result.status_code, 204)
             self.assertEqual(len(self.mock_events), 1)
+
+    def test_bad_request(self):
+        """Test on valid input"""
+        with patch("app.app.db.session.add", self.mocked_db_session_add), patch(
+            "app.app.db.session.commit", self.mocked_db_session_commit
+        ), patch("app.models.Contact.query") as mocked_query:
+            mocked_query.get = self.mocked_contact_query_get
+            result = add_new_event(
+                {
+                    "activity": "fishing",
+                    "start_time": datetime.datetime.now(),
+                    "period": "-1",
+                    "contact_id": "2000",
+                },
+                self.mock_user,
+            )
+            self.assertEqual(result.status_code, 400)
+            self.assertEqual(len(self.mock_events), 0)
